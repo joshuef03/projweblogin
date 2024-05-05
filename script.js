f9af0a3c8744ef70001af43a168d0da92b54a030
joshuef03 <138251847+joshuef03@users.noreply.github.com>
2024-05-05 14:39:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const info_btn = document.getElementsByClassName("info-btn")
+for (let i = 0; i < info_btn.length; i++) {
+  info_btn[i].onclick = () => {
+    document.querySelector(".container").classList.toggle("log-in");
+  }; 
+}
+
